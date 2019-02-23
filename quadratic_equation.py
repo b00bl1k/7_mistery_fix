d9ec70d908e2d914e2a92ea95133d4c6a45ff003
Alexey Ryabov <6006l1k@gmail.com>
2019-02-23 13:01:37 +0300

    Fix order of condition processing

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,13 +3,14 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    if discriminant > 0:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
-        return root1, root2
+
+    if discriminant < 0:
+        return None, None
+
+    root1 = (-b - sqrt(discriminant)) / (2 * a)
+    root2 = (-b + sqrt(discriminant)) / (2 * a)
 
     if discriminant == 0:
-        root1 = -b / (2 * a)
         return root1, None
 
-    return None, None
+    return root1, root2
